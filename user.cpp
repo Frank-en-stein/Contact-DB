95418a0d995ebe1650a86984eafa00ec5554f1c2
Frank-en-stein <smfaisal1648@gmail.com>
2018-08-14 01:38:16 +0600

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -30,12 +30,13 @@ public:
 
 	bstnode* push(int n, datanode* d, bstnode* curr, bstnode* &res) {
 		if (!curr) return res = new bstnode(d, n, nullptr, nullptr);
-		if (n <= curr->val) curr->left = push(n, d, curr->left, res), curr->left->par = curr;
-		else curr->right = push(n, d, curr->right, res), curr->right->par = curr;
+		if (n <= curr->val) { curr->left = push(n, d, curr->left, res); curr->left->par = curr; }
+		else { curr->right = push(n, d, curr->right, res); curr->right->par = curr; }
+		return curr;
 	}
 	datanode* remove_closest(int tr) {
 		bstnode* curr = root;
-		int d = MAX+10;
+		int d = MAX + 10;
 		bstnode* r = root;
 		while (curr) {
 			if (abs(tr - curr->val) < d) {
@@ -45,7 +46,11 @@ public:
 			if (tr < curr->val) curr = curr->left;
 			else curr = curr->right;
 		}
-		return remove(r);
+		if(r) return remove(r);
+		else {
+			int uu = 0;
+			return nullptr;
+		}
 	}
 	datanode* remove_smaller(int tr) {
 		bstnode* curr = root;
@@ -63,6 +68,9 @@ public:
 		else return nullptr;
 	}
 	datanode* remove_greater(int tr) {
+		if (tr == 101) {
+			int uu = 0;
+		}
 		bstnode* curr = root;
 		int d = MAX + 10;
 		bstnode* r = nullptr;
@@ -74,12 +82,15 @@ public:
 			if (tr < curr->val) curr = curr->left;
 			else curr = curr->right;
 		}
-		if (r) return remove(r);
-		else return nullptr;
+		return remove(r);
 	}
 	datanode* remove(bstnode* tmp) {
-		if (tmp == root) {
-			root = nullptr;
+		if (!tmp->left && !tmp->right) {
+			if (tmp->par) {
+				if (tmp->par->left == tmp) tmp->par->left = nullptr;
+				else tmp->par->right = nullptr;
+			}
+			else root = nullptr;
 			return tmp->data;
 		}
 		bstnode* curr = tmp;
@@ -87,13 +98,27 @@ public:
 		if (curr->right) {
 			pred = curr->right;
 			while (pred->left) pred = pred->left;
-			pred->left = curr->left;
-			if (curr->right != pred) pred->right = curr->right;
+			if (pred == curr->right) {
+				pred->left = curr->left;
+				if (curr->left) curr->left->par = pred;
+			}
+			else {
+				pred->par->left = pred->right;
+				if (pred->right) pred->right->par = pred->par;
+
+				pred->right = curr->right;
+				curr->right->par = pred;
+				pred->left = curr->left;
+				if (curr->left) curr->left->par = pred;
+			}
 		}
-		else pred = curr->left;
-		if (!curr->par) root = pred;
-		else if (curr->par->left == curr) curr->par->left = pred;
-		else curr->par->right = pred;
+		else {
+			pred = curr->left;
+			if (curr->left) curr->left->par = curr->par;
+		}
+		if (curr->par) if (curr->par->left == curr) curr->par->left = pred; else curr->par->right = pred;
+		pred->par = curr->par;
+		if (curr == root) root = pred;
 
 		return curr->data;
 	}
@@ -142,7 +167,7 @@ bst* vsorted;
 int lasthead;
 bool islookingleft;
 
-void init(int track_size, int head){
+void init(int track_size, int head) {
 	fifo = new list();
 	//dsorted = new bst();
 	vsorted = new bst();
@@ -150,35 +175,36 @@ void init(int track_size, int head){
 	islookingleft = true;
 }
 
-void request(int track){
+void request(int track) {
 	datanode* d = new datanode();
 	d->val = track;
 	d->listnode = fifo->push(track, d);
 	//d->diffnode = dsorted->push(abs(lasthead - track), d);
 	vsorted->root = vsorted->push(track, d, vsorted->root, d->valnode);
+	vsorted->root->par = nullptr;
 }
 
-int fcfs(){
+int fcfs() {
 	datanode* r = fifo->pop();
 	//dsorted->remove(r->diffnode);
 	vsorted->remove(r->valnode);
 	return lasthead = r->val;
 }
 
-int sstf(){
+int sstf() {
 	datanode* r = vsorted->remove_closest(lasthead);
 	fifo->remove(r->listnode);
 	return lasthead = r->val;
 }
 
-int look(){
+int look() {
 	datanode* r = (islookingleft ? vsorted->remove_smaller(lasthead) : vsorted->remove_greater(lasthead));
 	if (!r) islookingleft = !islookingleft, r = (islookingleft ? vsorted->remove_smaller(lasthead) : vsorted->remove_greater(lasthead));
 	fifo->remove(r->listnode);
 	return lasthead = r->val;
 }
 
-int clook(){
+int clook() {
 	datanode* r = vsorted->remove_smaller(lasthead);
 	if (!r) r = vsorted->remove_smaller(MAX + 100);
 	fifo->remove(r->listnode);
