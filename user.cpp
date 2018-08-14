b12dfbe39f68d178af769066d98928fe0c8423d4
Frank-en-stein <smfaisal1648@gmail.com>
2018-08-14 23:40:23 +0600

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -1,213 +1,162 @@
-const int MAX = 100000;
+#include <malloc.h>
 
-inline int abs(int k) {
-	return (k < 0 ? -k : k);
+inline int kabs(int n) {
+	return (n < 0 ? -n : n);
 }
 
-struct lnode;
-struct bstnode;
+const int MAX = 100000;
+int count = 1000000009;
 
-struct datanode {
-	int val;
-	lnode* listnode;
-	bstnode* diffnode;
-	bstnode* valnode;
-};
+typedef struct node {
+	int val, prior, size;
+	struct node *l, *r;
+}node;
+typedef node* pnode;
+int sz(pnode t) {
+	return t ? t->size : 0;
+}
+void upd_sz(pnode t) {
+	if (t) t->size = sz(t->l) + 1 + sz(t->r);
+}
+void split(pnode t, pnode &l, pnode &r, int key) {
+	if (!t) l = r = nullptr;
+	else if (t->val <= key) split(t->r, t->r, r, key), l = t; //elem=key comes in l
+	else split(t->l, l, t->l, key), r = t;
+	upd_sz(t);
+}
+void merge(pnode &t, pnode l, pnode r) {
+	if (!l || !r) t = l ? l : r;
+	else if (l->prior > r->prior)merge(l->r, l->r, r), t = l;
+	else merge(r->l, l, r->l), t = r;
+	upd_sz(t);
+}
 
-struct bstnode {
-	int val;
-	datanode* data;
-	bstnode* left;
-	bstnode* right;
-	bstnode* par;
-	bstnode(datanode* d, int n, bstnode* l, bstnode* r) { data = d, val = n, left = l, right = r; }
-};
+void insert(pnode &t, pnode it) {
+	if (!t) t = it;
+	else if (it->prior>t->prior) split(t, it->l, it->r, it->val), t = it;
+	else insert(t->val <= it->val ? t->r : t->l, it);
+	upd_sz(t);
+}
+void erase(pnode &t, int key) {
+	if (!t)return;
+	else if (t->val == key) { pnode temp = t; merge(t, t->l, t->r); free(temp); }
+	else erase(t->val<key ? t->r : t->l, key);
+	upd_sz(t);
+}
+pnode makenode(int val) {
+	pnode ret = (pnode)malloc(sizeof(node));
+	ret->val = val; ret->size = 1; ret->prior = count--; ret->l = ret->r = NULL;
+	return ret;
+}
 
-class bst {
-public:
-	bstnode* root;
-	bst() { root = nullptr; }
-
-	bstnode* push(int n, datanode* d, bstnode* curr, bstnode* &res) {
-		if (!curr) return res = new bstnode(d, n, nullptr, nullptr);
-		if (n <= curr->val) { curr->left = push(n, d, curr->left, res); curr->left->par = curr; }
-		else { curr->right = push(n, d, curr->right, res); curr->right->par = curr; }
-		return curr;
-	}
-	datanode* remove_closest(int tr) {
-		bstnode* curr = root;
-		int d = MAX + 10;
-		bstnode* r = root;
-		while (curr) {
-			if (abs(tr - curr->val) < d) {
-				d = abs(tr - curr->val);
-				r = curr;
-			}
-			if (tr < curr->val) curr = curr->left;
-			else curr = curr->right;
-		}
-		if(r) return remove(r);
-		else {
-			int uu = 0;
-			return nullptr;
-		}
-	}
-	datanode* remove_smaller(int tr) {
-		bstnode* curr = root;
-		int d = MAX + 10;
-		bstnode* r = nullptr;
-		while (curr) {
-			if (tr >= curr->val && (tr - curr->val) < d) {
-				d = abs(tr - curr->val);
-				r = curr;
-			}
-			if (tr < curr->val) curr = curr->left;
-			else curr = curr->right;
-		}
-		if (r) return remove(r);
-		else return nullptr;
-	}
-	datanode* remove_greater(int tr) {
-		if (tr == 101) {
-			int uu = 0;
-		}
-		bstnode* curr = root;
-		int d = MAX + 10;
-		bstnode* r = nullptr;
-		while (curr) {
-			if (tr <= curr->val && (tr - curr->val) < d) {
-				d = abs(tr - curr->val);
-				r = curr;
-			}
-			if (tr < curr->val) curr = curr->left;
-			else curr = curr->right;
-		}
-		return remove(r);
-	}
-	datanode* remove(bstnode* tmp) {
-		if (!tmp->left && !tmp->right) {
-			if (tmp->par) {
-				if (tmp->par->left == tmp) tmp->par->left = nullptr;
-				else tmp->par->right = nullptr;
-			}
-			else root = nullptr;
-			return tmp->data;
-		}
-		bstnode* curr = tmp;
-		bstnode* pred = nullptr;
-		if (curr->right) {
-			pred = curr->right;
-			while (pred->left) pred = pred->left;
-			if (pred == curr->right) {
-				pred->left = curr->left;
-				if (curr->left) curr->left->par = pred;
-			}
-			else {
-				pred->par->left = pred->right;
-				if (pred->right) pred->right->par = pred->par;
-
-				pred->right = curr->right;
-				curr->right->par = pred;
-				pred->left = curr->left;
-				if (curr->left) curr->left->par = pred;
-			}
-		}
-		else {
-			pred = curr->left;
-			if (curr->left) curr->left->par = curr->par;
-		}
-		if (curr->par) if (curr->par->left == curr) curr->par->left = pred; else curr->par->right = pred;
-		pred->par = curr->par;
-		if (curr == root) root = pred;
-
-		return curr->data;
-	}
-};
-
-struct lnode {
-	int val;
-	datanode* data;
-	lnode* prev;
-	lnode* next;
-	lnode(datanode* d, int n, lnode* prv, lnode* nxt) {
-		data = d;
-		val = n;
-		prev = prv;
-		next = nxt;
-	}
-};
-
-class list {
-public:
-	lnode *head, *tail;
-	int count;
-	list() { head = new lnode(nullptr, -1, nullptr, nullptr); tail = head; count = 0; }
-	lnode* push(int n, datanode* d) {
-		lnode* tmp = new lnode(d, n, tail, nullptr);
-		tail->next = tmp;
-		tail = tmp;
-		return tmp;
-	}
-	datanode* pop() {
-		lnode* tmp = head->next;
-		datanode* r = tmp->data;
-		remove(tmp);
-		return r;
-	}
-	void remove(lnode* node) {
-		if (node == tail) tail = node->prev;
-		node->prev->next = node->next;
-		if (node->next) node->next->prev = node->prev;
-	}
-};
-
-list* fifo;
-//bst* dsorted;
-bst* vsorted;
+pnode root;
+bool lookingLeft;
 int lasthead;
-bool islookingleft;
+
+int remove_closest(int tr) {
+	pnode curr = root;
+	int d = MAX + 10;
+	pnode r = nullptr;
+	while (curr) {
+		if (kabs(tr - curr->val) < d || (kabs(tr - curr->val) == d && curr->val < r->val)) {
+			d = kabs(tr - curr->val);
+			r = curr;
+		}
+		if (tr < curr->val) curr = curr->l;
+		else curr = curr->r;
+	}
+	if (r) {
+		int res = r->val;
+		erase(root, res);
+		return res;
+	}
+	else {
+		return MAX+10;
+	}
+}
+int remove_smaller(int tr) {
+	pnode curr = root;
+	int d = MAX + 10;
+	pnode r = nullptr;
+	while (curr) {
+		if (tr >= curr->val && (tr - curr->val) < d) {
+			d = kabs(tr - curr->val);
+			r = curr;
+		}
+		if (tr < curr->val) curr = curr->l;
+		else curr = curr->r;
+	}
+	if (r) {
+		int res = r->val;
+		erase(root, res);
+		return res;
+	}
+	else {
+		return MAX + 10;
+	}
+}
+int remove_greater(int tr) {
+	if (tr == 101) {
+		int uu = 0;
+	}
+	pnode curr = root;
+	int d = MAX + 10;
+	pnode r = nullptr;
+	while (curr) {
+		if (tr <= curr->val && (tr - curr->val) < d) {
+			d = kabs(tr - curr->val);
+			r = curr;
+		}
+		if (tr < curr->val) curr = curr->l;
+		else curr = curr->r;
+	}
+	if (r) {
+		int res = r->val;
+		erase(root, res);
+		return res;
+	}
+	else {
+		return MAX + 10;
+	}
+}
 
 void init(int track_size, int head) {
-	fifo = new list();
-	//dsorted = new bst();
-	vsorted = new bst();
+	count = 1000000009;
+	root = nullptr;
+	lookingLeft = true;
 	lasthead = head;
-	islookingleft = true;
 }
 
 void request(int track) {
-	datanode* d = new datanode();
-	d->val = track;
-	d->listnode = fifo->push(track, d);
-	//d->diffnode = dsorted->push(abs(lasthead - track), d);
-	vsorted->root = vsorted->push(track, d, vsorted->root, d->valnode);
-	vsorted->root->par = nullptr;
+	pnode tmp = makenode(track);
+	insert(root, tmp);
 }
 
 int fcfs() {
-	datanode* r = fifo->pop();
-	//dsorted->remove(r->diffnode);
-	vsorted->remove(r->valnode);
-	return lasthead = r->val;
+	int r = root->val;
+	erase(root, r);
+	return lasthead = r;
 }
 
 int sstf() {
-	datanode* r = vsorted->remove_closest(lasthead);
-	fifo->remove(r->listnode);
-	return lasthead = r->val;
+	int r = remove_closest(lasthead);
+	return lasthead = r;
 }
 
 int look() {
-	datanode* r = (islookingleft ? vsorted->remove_smaller(lasthead) : vsorted->remove_greater(lasthead));
-	if (!r) islookingleft = !islookingleft, r = (islookingleft ? vsorted->remove_smaller(lasthead) : vsorted->remove_greater(lasthead));
-	fifo->remove(r->listnode);
-	return lasthead = r->val;
+	int r = (lookingLeft ? remove_smaller(lasthead) : remove_greater(lasthead));
+	if (r == MAX + 10) {
+		lookingLeft = !lookingLeft;
+		r = (lookingLeft ? remove_smaller(lasthead) : remove_greater(lasthead));
+	}
+	return lasthead = r;
 }
 
 int clook() {
-	datanode* r = vsorted->remove_smaller(lasthead);
-	if (!r) r = vsorted->remove_smaller(MAX + 100);
-	fifo->remove(r->listnode);
-	return lasthead = r->val;
+	int r = remove_smaller(lasthead);
+	if (r == MAX + 10) {
+		r = remove_smaller(MAX+10);
+	}
+	return lasthead = r;
 }
 
