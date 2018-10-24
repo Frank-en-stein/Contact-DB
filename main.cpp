f4c2f082616bc6c7ccab7f508a6aa7034ba00d04
Frank-en-stein <smfaisal1648@gmail.com>
2018-10-25 00:55:23 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,24 +10,36 @@ class mymap {
 public:
 	mymap(V const & val) {
 		m_map.insert(m_map.begin(), std::make_pair(std::numeric_limits<K>::min(), val));
-		m_map.insert(m_map.begin(), std::make_pair(std::numeric_limits<K>::max(), val));
 	}
 	V & operator [] (K const & key) {
 		return (--m_map.upper_bound(key))->second;
 	}
 
-	void assign(K const & keyBegin, K const & keyEnd, V const & val) {
-
+	void assign(K const & keyBegins, K const & keyEnds, V const & val) {
+        K keyBegin = keyBegins;
+        K keyEnd = keyEnds;
 		if (keyBegin >= keyEnd) return;
+		if (keyBegin < std::numeric_limits<K>::min()) keyBegin = std::numeric_limits<K>::min();
+		if (keyEnd > std::numeric_limits<K>::max()) keyEnd = std::numeric_limits<K>::max();
+
         auto l = m_map.lower_bound(keyBegin);
-        if (l == m_map.end()) l = m_map.begin();
         auto r = m_map.lower_bound(keyEnd);
-        //auto lprev = --l; ++l;
-        //auto rpost = ++r; --r;
+        auto tmpR = r;
+        auto defV = (--tmpR)->second;
         cout<<l->first<<" "<<r->first<<endl;
-        if (l->first < r->first) m_map.erase(l, r);
+        // NOTE: handle begin and end cases
+        if (l != r) {
+            if (l == m_map.begin() || r == m_map.end()) m_map.erase(l, r);
+            else if (l->first < r->first) m_map.erase(l, r);
+        }
+        auto ll = l, rr = r;
 
+        if (rr == m_map.end()) m_map[keyEnd] = defV;
+        else if (rr->first != keyEnd) m_map[keyEnd] = defV;
 
+        --ll;
+        if (ll == m_map.begin()) m_map[keyBegin] = val;
+        else if (ll->second != val) m_map[keyBegin] = val;
 	}
 
 	void test_interval_map() {
@@ -42,7 +54,7 @@ public:
 	void show() {
 		std::cout << "show" << std::endl;
 		for(auto entry : m_map) {
-			std::cout << entry.first << entry.second << std::endl;
+			std::cout << entry.first << " " << entry.second << std::endl;
 		}
 	}
 
@@ -51,9 +63,12 @@ private:
 };
 
 int main() {
-	mymap<unsigned int, char> imap {'a'};
-	imap.test_interval_map();
-    imap.assign(0,10,'m');
+	mymap<unsigned int, char> imap {'#'};
+	//imap.test_interval_map();
+    imap.assign(1,7,'m');
+    imap.assign(7, 10, 'm');
+    imap.assign(0,68,'o');
+    cout<<imap[1001]<<endl;
     imap.show();
 }
 
