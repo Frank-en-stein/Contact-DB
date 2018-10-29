c19d8b6ebfeb92cbf03e38f6de348b930edcfcc5
Frank-en-stein <smfaisal1648@gmail.com>
2018-10-30 01:58:41 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,22 @@
 #include <limits>
 #include <iostream>
 #include <iterator>
-
-using namespace std;
+#include <string>
 
 template <typename K, typename V>
-class mymap {
+class interval_map {
 public:
-	mymap(V const & val) {
-		m_map.insert(m_map.begin(), std::make_pair(std::numeric_limits<K>::min(), val));
+	interval_map(V const& val) {
+		m_map.insert(m_map.end(), std::make_pair(std::numeric_limits<K>::lowest(), val));
 	}
-	V & operator [] (K const & key) {
+	// look-up of the value associated with key
+	V const& operator[](K const& key) const {
 		return (--m_map.upper_bound(key))->second;
 	}
 
-	void assign(K const & keyBegins, K const & keyEnds, V const & val) {
-		K keyBegin = keyBegins;
-		K keyEnd = keyEnds;
-		if (keyBegin >= keyEnd) return;
-		if (keyBegin < std::numeric_limits<K>::min()) keyBegin = std::numeric_limits<K>::min();
-		if (keyEnd > std::numeric_limits<K>::max()) keyEnd = std::numeric_limits<K>::max();
+	void assign(K const & keyBegin, K const & keyEnd, V const & val) {
+		if (!(keyBegin < keyEnd)) return;
+		if (keyBegin < std::numeric_limits<K>::lowest()) return;
 
 		auto l = m_map.lower_bound(keyBegin);
 		auto r = m_map.lower_bound(keyEnd);
@@ -31,72 +28,51 @@ public:
 			else if (l->first < r->first) m_map.erase(l, r);
 		}
 
-		auto ll = m_map.insert(std::pair<K, V>(keyBegin, val));
-		auto pointer = ll.first;
-		while (pointer != m_map.begin()) {
-			if (prev(pointer)->second != pointer->second) break;
+		auto ll = m_map.insert(r, std::pair<K, V>(keyBegin, val));
+		while (ll != m_map.begin()) {
+			if (!(std::prev(ll)->second == ll->second)) break;
 			else {
-				auto tmp = pointer;
-				pointer = prev(pointer);
+				auto tmp = ll;
+				ll = std::prev(ll);
 				m_map.erase(tmp);
 			}
 		}
 
 		if (r == m_map.end()) {
-			auto rr = m_map.insert(std::pair<K, V>(keyEnd, defV));
-			_clear(rr.first);
-		}
-		else if (r->first != keyEnd && r->second != defV) {
-			auto rr = m_map.insert(std::pair<K, V>(keyEnd, defV));
-			_clear(rr.first);
-		}
-		else if (next(pointer) != m_map.end()) {
-			if (pointer->second == next(pointer)->second) m_map.erase(next(pointer));
-		}
-	}
-
-	template<typename Iterator>
-	void _clear(Iterator pointer) {
-		while (next(pointer) != m_map.end()) {
-			if (next(pointer)->second != pointer->second) break;
-			else pointer = std::next(pointer);
-		}
-		while (pointer != m_map.begin()) {
-			if (prev(pointer)->second != pointer->second) break;
-			else {
-				auto tmp = pointer;
-				pointer = std::prev(pointer);
-				m_map.erase(tmp);
+			auto rr = m_map.insert(r, std::pair<K, V>(keyEnd, defV));
+			while (std::next(rr) != m_map.end()) {
+				if (!(std::next(rr)->second == rr->second)) break;
+				else rr = std::next(rr);
+			}
+			while (rr != m_map.begin()) {
+				if (!(std::prev(rr)->second == rr->second)) break;
+				else {
+					auto tmp = rr;
+					rr = std::prev(rr);
+					m_map.erase(tmp);
+				}
 			}
 		}
-	}
-
-	template<typename Iterator>
-	void _clearDuplicates(Iterator a, Iterator b) {
-		if (a == b) return;
-		V val = a->second;
-		Iterator curr = a;
-		do {
-			curr = next(curr);
-			if (curr->second == val) {
-				Iterator tmp = curr;
-				m_map.erase(tmp);
+		else if (keyEnd < r->first && !(r->second == defV)) {
+			auto rr = m_map.insert(r, std::pair<K, V>(keyEnd, defV));
+			while (std::next(rr) != m_map.end()) {
+				if (!(std::next(rr)->second == rr->second)) break;
+				else rr = std::next(rr);
 			}
-			else {
-				val = curr->second;
+			while (rr != m_map.begin()) {
+				if (!(std::prev(rr)->second == rr->second)) break;
+				else {
+					auto tmp = rr;
+					rr = std::prev(rr);
+					m_map.erase(tmp);
+				}
 			}
-		} while (curr != b);
+		}
+		else if (ll != m_map.end()) if(std::next(ll) != m_map.end()) {
+			if (ll->second == std::next(ll)->second) m_map.erase(std::next(ll));
+		}
 	}
 
-	void test_interval_map() {
-		m_map.insert(std::make_pair(1, 'b'));
-		m_map.insert(std::make_pair(4, 'c'));
-		m_map.insert(std::make_pair(7, 'd'));
-		m_map.insert(std::make_pair(10, 'e'));
-		m_map.insert(std::make_pair(13, 'a'));
-	}
-
-	// a print function for debugging
 	void show() {
 		std::cout << "show" << std::endl;
 		for (auto entry : m_map) {
@@ -109,11 +85,13 @@ private:
 };
 
 int main() {
-	mymap<unsigned int, char> imap{ '#' };
-	//imap.test_interval_map();
+	interval_map<unsigned int, std::string> imap{ "#" };
 	
-	imap.assign(-1, 69, 'm');
-	imap.assign(0, 7, 'm');
+	
+	imap.assign(80, 458, "m");
+	imap.assign(85, 100, "p");
+	imap.assign(90, 95, "q");
+	imap.assign(91, 93, "r");
 	imap.show();
 }
 
