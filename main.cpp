a360fc0e4223abcdb59bfbd2d71fa048aa9583a3
Frank-en-stein <smfaisal1648@gmail.com>
2018-10-31 17:13:34 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,98 +1,69 @@
-#include <map>
-#include <limits>
-#include <iostream>
-#include <iterator>
-#include <string>
+#include <stdio.h>
 
-template <typename K, typename V>
-class interval_map {
-public:
-	interval_map(V const& val) {
-		m_map.insert(m_map.end(), std::make_pair(std::numeric_limits<K>::lowest(), val));
-	}
-	// look-up of the value associated with key
-	V const& operator[](K const& key) const {
-		return (--m_map.upper_bound(key))->second;
-	}
+#define MAX_PRICE  16000
+#define MAX_WEIGTH 4000
+#define MAX_CALLS 100000
+#define MAX_DP 100
+#define MAX_DW 100
 
-	void assign(K const & keyBegin, K const & keyEnd, V const & val) {
-		if (!(keyBegin < keyEnd)) return;
-		if (keyBegin < std::numeric_limits<K>::lowest()) return;
-
-		auto l = m_map.lower_bound(keyBegin);
-		auto r = m_map.lower_bound(keyEnd);
-		auto defV = std::prev(r)->second;
-		//cout << l->first << " " << r->first << endl;
-		if (l != r) {
-			if (l == m_map.begin() || r == m_map.end()) m_map.erase(l, r);
-			else if (l->first < r->first) m_map.erase(l, r);
-		}
-
-		auto ll = m_map.insert(r, std::pair<K, V>(keyBegin, val));
-		while (ll != m_map.begin()) {
-			if (!(std::prev(ll)->second == ll->second)) break;
-			else {
-				auto tmp = ll;
-				ll = std::prev(ll);
-				m_map.erase(tmp);
-			}
-		}
-
-		if (r == m_map.end()) {
-			auto rr = m_map.insert(r, std::pair<K, V>(keyEnd, defV));
-			while (std::next(rr) != m_map.end()) {
-				if (!(std::next(rr)->second == rr->second)) break;
-				else rr = std::next(rr);
-			}
-			while (rr != m_map.begin()) {
-				if (!(std::prev(rr)->second == rr->second)) break;
-				else {
-					auto tmp = rr;
-					rr = std::prev(rr);
-					m_map.erase(tmp);
-				}
-			}
-		}
-		else if (keyEnd < r->first && !(r->second == defV)) {
-			auto rr = m_map.insert(r, std::pair<K, V>(keyEnd, defV));
-			while (std::next(rr) != m_map.end()) {
-				if (!(std::next(rr)->second == rr->second)) break;
-				else rr = std::next(rr);
-			}
-			while (rr != m_map.begin()) {
-				if (!(std::prev(rr)->second == rr->second)) break;
-				else {
-					auto tmp = rr;
-					rr = std::prev(rr);
-					m_map.erase(tmp);
-				}
-			}
-		}
-		else if (ll != m_map.end()) if(std::next(ll) != m_map.end()) {
-			if (ll->second == std::next(ll)->second) m_map.erase(std::next(ll));
-		}
-	}
-
-	void show() {
-		std::cout << "show" << std::endl;
-		for (auto entry : m_map) {
-			std::cout << entry.first << " " << entry.second << std::endl;
-		}
-	}
-
-private:
-	std::map<K, V> m_map;
-};
-
-int main() {
-	interval_map<unsigned int, std::string> imap{ "#" };
-	
-	
-	imap.assign(80, 458, "m");
-	imap.assign(85, 100, "p");
-	imap.assign(90, 95, "q");
-	imap.assign(91, 93, "r");
-	imap.show();
+typedef unsigned int uint;
+static uint seed;
+static uint mrand(uint num)
+{
+	seed = seed * 1103515245 + 37209;
+	return ((seed >> 8) % num);
 }
 
+void init();
+void add(int price, int weight);
+int search(int price1, int price2, int weight1, int weight2);
 
+int test(int ncmd)
+{
+	init();
+	int sum = 0;
+	int cmd;
+
+	for (int i = 0; i < ncmd; ++i)
+	{
+		cmd = mrand(8);
+		if (cmd == 0) // search
+		{
+			int p1 = mrand(MAX_PRICE);
+			int p2 = p1 + mrand(MAX_DP);
+			if (p2 >= MAX_PRICE) p2 = MAX_PRICE - 1;
+
+			int w1 = mrand(MAX_WEIGTH);
+			int w2 = w1 + mrand(MAX_DW);
+			if (w2 >= MAX_WEIGTH) w2 = MAX_WEIGTH - 1;
+
+			sum += search(p1, p2, w1, w2);
+			//printf("%d\n", i);
+		}
+		else // add
+		{
+			int w = mrand(MAX_WEIGTH);
+			int p = mrand(MAX_PRICE);
+			add(p, w);
+		}
+	}
+	return sum;
+}
+
+int main()
+{
+	freopen("Contact_DB.txt", "r", stdin);
+	setbuf(stdout, NULL);
+
+	int ntests = 0;
+	scanf("%i", &ntests);
+	for (int t = 0; t < ntests; ++t)
+	{
+		int ncmd, sum;
+		scanf("%u %i %i", &seed, &ncmd, &sum);
+		int ll = test(ncmd);
+		bool correct = (sum == ll);
+		printf("#%i %i\n", t + 1, correct ? 100 : 0);
+	}
+	return 0;
+}
