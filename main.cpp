c80608ad1a839eec02e23b624d38f1342fc610cc
Frank-en-stein <smfaisal1648@gmail.com>
2019-01-09 13:52:51 +0600

    well yeah

--- main.cpp
+++ main.cpp
@@ -1,69 +1,85 @@
-#include <stdio.h>
+#include<stdio.h>
 
-#define MAX_PRICE  16000
-#define MAX_WEIGTH 4000
-#define MAX_CALLS 100000
-#define MAX_DP 100
-#define MAX_DW 100
+#define UP 0
+#define RIGHT 1
+#define DOWN 2
+#define LEFT 3
+#define LIMIT 100
 
-typedef unsigned int uint;
-static uint seed;
-static uint mrand(uint num)
+extern void solve(int board[4][4]);
+
+int EmptyRow, EmptyCol;
+int board[4][4];
+
+bool move(int dir)
 {
-	seed = seed * 1103515245 + 37209;
-	return ((seed >> 8) % num);
-}
-
-void init();
-void add(int price, int weight);
-int search(int price1, int price2, int weight1, int weight2);
-
-int test(int ncmd)
-{
-	init();
-	int sum = 0;
-	int cmd;
-
-	for (int i = 0; i < ncmd; ++i)
-	{
-		cmd = mrand(8);
-		if (cmd == 0) // search
-		{
-			int p1 = mrand(MAX_PRICE);
-			int p2 = p1 + mrand(MAX_DP);
-			if (p2 >= MAX_PRICE) p2 = MAX_PRICE - 1;
-
-			int w1 = mrand(MAX_WEIGTH);
-			int w2 = w1 + mrand(MAX_DW);
-			if (w2 >= MAX_WEIGTH) w2 = MAX_WEIGTH - 1;
-
-			sum += search(p1, p2, w1, w2);
-			//printf("%d\n", i);
-		}
-		else // add
-		{
-			int w = mrand(MAX_WEIGTH);
-			int p = mrand(MAX_PRICE);
-			add(p, w);
-		}
+	switch (dir) {
+	case UP:
+		if (3 == EmptyRow)
+			return false;
+		board[EmptyRow][EmptyCol] = board[EmptyRow + 1][EmptyCol];
+		board[EmptyRow + 1][EmptyCol] = 0;
+		EmptyRow++;
+		break;
+	case RIGHT:
+		if (0 == EmptyCol)
+			return false;
+		board[EmptyRow][EmptyCol] = board[EmptyRow][EmptyCol - 1];
+		board[EmptyRow][EmptyCol - 1] = 0;
+		EmptyCol--;
+		break;
+	case DOWN:
+		if (0 == EmptyRow)
+			return false;
+		board[EmptyRow][EmptyCol] = board[EmptyRow - 1][EmptyCol];
+		board[EmptyRow - 1][EmptyCol] = 0;
+		EmptyRow--;
+		break;
+	case LEFT:
+		if (3 == EmptyCol)
+			return false;
+		board[EmptyRow][EmptyCol] = board[EmptyRow][EmptyCol + 1];
+		board[EmptyRow][EmptyCol + 1] = 0;
+		EmptyCol++;
+		break;
 	}
-	return sum;
+	return true;
 }
 
 int main()
 {
 	freopen("Contact_DB.txt", "r", stdin);
-	setbuf(stdout, NULL);
-
-	int ntests = 0;
-	scanf("%i", &ntests);
-	for (int t = 0; t < ntests; ++t)
-	{
-		int ncmd, sum;
-		scanf("%u %i %i", &seed, &ncmd, &sum);
-		int ll = test(ncmd);
-		bool correct = (sum == ll);
-		printf("#%i %i\n", t + 1, correct ? 100 : 0);
+	int T;
+	int Case;
+	int boardCopy[4][4];
+	int row, col;
+	scanf("%d", &T);
+	for (Case = 1; Case <= T; Case++) {
+		for (row = 0; row < 4; row++) {
+			for (col = 0; col < 4; col++) {
+				scanf("%d", &board[row][col]);
+				boardCopy[row][col] = board[row][col];
+				if (0 == board[row][col]) {
+					EmptyRow = row;
+					EmptyCol = col;
+				}
+			}
+		}
+		int movecount = 0;
+		solve(boardCopy);
+		int match = 0;
+		for (row = 0; row < 4; row++) {
+			for (col = 0; col < 4; col++) {
+				if (row * 4 + col + 1 == board[row][col])
+					match++;
+			}
+		}
+		if (0 == board[3][3])
+			match++;
+		if (4 <= match)
+			printf("#%d Pass\n", Case);
+		else
+			printf("#%d %d\n", Case, match);
 	}
 	return 0;
 }
